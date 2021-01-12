066d0669dab9f2884e27fd39fb7d1ced3d77622e
Blake Julander <13lakejulander@gmail.com>
2021-01-12 11:17:37 -0700

    commit tree a9220359c97da6b1174cb8b8c20c2f0deb15dd95

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(phrase)
          "I LOVE YOU TOO PUMPKIN!"
     elsif phrase == phrase.upcase
          "NO, NOT SINCE 1938!"
-    elsif phrase == phrase.downcase
+    else
          "HUH?! SPEAK UP, SONNY!"
     end
 end
